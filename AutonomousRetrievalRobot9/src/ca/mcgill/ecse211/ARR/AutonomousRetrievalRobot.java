43e0c4a27bc92fd13c1e31e795bbd477afee158b
Julien Lesaffre <julien.lesaffre@mail.mcgill.ca>
2018-10-26 11:44:01 -0400

    main class added

---
+++ AutonomousRetrievalRobot9/src/ca/mcgill/ecse211/ARR/AutonomousRetrievalRobot.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ca.mcgill.ecse211.ARR;
+
+/**
+ * @author JulienLesaffre
+ *
+ */
+public class AutonomousRetrievalRobot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

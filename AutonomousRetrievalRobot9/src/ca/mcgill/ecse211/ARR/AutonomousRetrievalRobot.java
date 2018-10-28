b5193aedc3c08a88ab6828cafceebbdfc52709e5
Julien Lesaffre <julien.lesaffre@mail.mcgill.ca>
2018-10-28 15:59:40 -0400

    added main parameters, Navigation class and Display class

--- AutonomousRetrievalRobot9/src/ca/mcgill/ecse211/ARR/AutonomousRetrievalRobot.java
+++ AutonomousRetrievalRobot9/src/ca/mcgill/ecse211/ARR/AutonomousRetrievalRobot.java
@@ -8,6 +8,25 @@ package ca.mcgill.ecse211.ARR;
  *
  */
 public class AutonomousRetrievalRobot {
+	
+	// Game parameters (will be provided with Wifi Class)
+	private static final int RedTeam = 0; // Team starting out from red zone
+	private static final int GreenTeam = 0; // Team starting out from green zone
+	private static final int[] RedCorner = {0,0}; // Starting corner for red team
+	private static final int[] GreenCorner = {0,0}; // Starting corner for green team
+	private static final int[] Red_LL = {0,0}; // lower left hand corner of Red Zone
+	private static final int[] Red_UR = {0,0}; // upper right hand corner of Red Zone
+	private static final int[] Green_LL = {0,0}; // lower left hand corner of Green Zone
+	private static final int[] Green_UR = {0,0}; // upper right hand corner of Green Zone
+	private static final int[] BRR_LL = {0,0}; // lower left hand corner of the red tunnel footprint
+	private static final int[] BRR_UR = {0,0}; // upper right hand corner of the red tunnel footprint
+	private static final int[] BRG_LL = {0,0}; // lower left hand corner of the green tunnel footprint
+	private static final int[] BRG_UR = {0,0}; // upper right hand corner of the green tunnel footprint
+	private static final int[] TR_LL = {0,0}; // lower left hand corner of the red player ring set
+	private static final int[] TR_UR = {0,0}; // upper right hand corner of the red player ring set
+	private static final int[] TG_LL = {0,0}; // lower left hand corner of the green player ring set
+	private static final int[] TG_UR = {0,0}; // upper right hand corner of the green player ring set
+
 
 	/**
 	 * @param args
